376ef1ea58160e63584d9ae15f3c6586393fa35a
haejoon <heidi990828@naver.com>
2023-04-28 22:59:01 +0900

    cutout num_holes=12

--- data.py
+++ data.py
@@ -65,7 +65,7 @@ def get_transforms(CFG):
                             A.ColorJitter(p=1.0),
                             A.RandomRotate90(p=1.0),
                             A.Affine(scale=(0.8,1.2), translate_percent=(0.0,0.3), shear=[-45,45]),
-                            A.Cutout(num_holes=8, max_h_size=10, max_w_size=10, fill_value=0, always_apply=False, p=0.5),
+                            A.Cutout(num_holes=12, max_h_size=10, max_w_size=10, fill_value=0, always_apply=False, p=0.5),
                             #A.AdvancedBlur(),
                             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0), #0-255를 0-1로 바꾸는 과정 : 데이터셋에 맞춰서 변환해줌
                             # TODO: data augmentation can come here
